569579b9902605a45441481b049d8d0b4facfd9b
Kevin RAMAGE <kevinramage@hotmail.fr>
2020-05-22 15:51:59 +0200

    Change the behaviour route

--- src/business/route.ts
+++ src/business/route.ts
@@ -19,11 +19,11 @@ export class Route {
         var code = "";
         code += util.format("\t\tthis.router.route(\"%s/_resetCounter\").post(%s._%s_resetCounter);\n", this.pingPath, service.mockName, service.methodName);
         code += util.format("\t\tthis.router.route(\"%s/_ping\").get(%s._%s_ping);\n", this.pingPath, service.mockName, service.methodName);
-        code += util.format("\t\tthis.router.route(\"%s/_behaviour/:name\").get(%s._%s_getBehaviour);\n", this.path, service.mockName, service.methodName);
-        code += util.format("\t\tthis.router.route(\"%s/_behaviour/\").get(%s._%s_getAllBehaviours);\n", this.path, service.mockName, service.methodName);
-        code += util.format("\t\tthis.router.route(\"%s/_behaviour/\").post(%s._%s_createBehaviour);\n", this.path, service.mockName, service.methodName);
-        code += util.format("\t\tthis.router.route(\"%s/_behaviour/:name\").delete(%s._%s_deleteBehaviour);\n", this.path, service.mockName, service.methodName);
-        code += util.format("\t\tthis.router.route(\"%s/_behaviour\").delete(%s._%s_deleteAllBehaviours);\n", this.path, service.mockName, service.methodName);
+        code += util.format("\t\tthis.router.route(\"/api/v1/%s/_behaviour/:name\").get(%s._%s_getBehaviour);\n", service.methodName, service.mockName, service.methodName);
+        code += util.format("\t\tthis.router.route(\"/api/v1/%s/_behaviour/\").get(%s._%s_getAllBehaviours);\n", service.methodName, service.mockName, service.methodName);
+        code += util.format("\t\tthis.router.route(\"/api/v1/%s/_behaviour/\").post(%s._%s_createBehaviour);\n", service.methodName, service.mockName, service.methodName);
+        code += util.format("\t\tthis.router.route(\"/api/v1/%s/_behaviour/:name\").delete(%s._%s_deleteBehaviour);\n", service.methodName, service.mockName, service.methodName);
+        code += util.format("\t\tthis.router.route(\"/api/v1/%s/_behaviour\").delete(%s._%s_deleteAllBehaviours);\n", service.methodName, service.mockName, service.methodName);
         code += util.format("\t\tthis.router.route(\"%s\").%s(%s.%s);\n", this.path, this.method.toLowerCase(), mockName, service.methodName);
         return code;
     }
